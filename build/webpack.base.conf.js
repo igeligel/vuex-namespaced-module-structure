663c87c22618281ebfa10605bc2f9cbd34e40061
igeligel <kevinigeligeligel@gmail.com>
2018-05-03 20:35:12 +0200

    chore(dependencies): upgrade dependencies
    upgrade several dependencies and migrate breaking vue-loader upgrade

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const { VueLoaderPlugin } = require('vue-loader')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -26,6 +27,9 @@ module.exports = {
       '@': resolve('src'),
     }
   },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   module: {
     rules: [
       {
